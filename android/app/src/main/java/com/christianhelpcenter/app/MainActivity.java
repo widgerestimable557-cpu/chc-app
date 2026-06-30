a97aca3601c16138a6b4a2952b6848b34f4f184d
widgerestimable557-cpu <widgerestimable557@gmail.com>
2026-06-30 19:37:17 +0000

    debug: show FCM token in copyable dialog on startup

--- android/app/src/main/java/com/christianhelpcenter/app/MainActivity.java
+++ android/app/src/main/java/com/christianhelpcenter/app/MainActivity.java
@@ -104,7 +104,13 @@ public class MainActivity extends Activity {
                         final String tok = task.getResult();
                         android.util.Log.d("CHC_FCM_TOKEN", tok);
                         runOnUiThread(new Runnable() { @Override public void run() {
-                            android.widget.Toast.makeText(MainActivity.this, "Token FCM copie dans logcat", android.widget.Toast.LENGTH_LONG).show();
+                            android.widget.EditText et = new android.widget.EditText(MainActivity.this);
+                            et.setText(tok);
+                            new android.app.AlertDialog.Builder(MainActivity.this)
+                                .setTitle("Token FCM (copier)")
+                                .setView(et)
+                                .setPositiveButton("Fermer", null)
+                                .show();
                         }});
                     }
                 }
